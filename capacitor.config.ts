e272736d76479dcec9932db5b4fa38a9c7dbe292
Bautista Dev <batubasilio@hotmail.com>
2023-06-27 20:57:22 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'resto-Front',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
