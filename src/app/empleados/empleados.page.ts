c68b76e79112b1d18ea51d0e456a3b03147dd4e4
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-06-28 16:56:43 -0300

    feat:Implementacion de la pagina de empleados

--- src/app/empleados/empleados.page.ts
+++ src/app/empleados/empleados.page.ts
@@ -2,7 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import axios from 'axios';
 
+interface users{
+  id:Number
+  userName:string,
+  nombre:string,
+  apellido:string,
+  dni:Number,
+  rol:{
+    id:Number,
+    rol:string
+  }
+}
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.page.html',
@@ -12,7 +24,25 @@ import { IonicModule } from '@ionic/angular';
 })
 export class EmpleadosPage implements OnInit {
 
-  constructor() { }
+  users:users[]=[];
+
+  async retriveAllEmployed(){
+    var employed = await axios.get("http://localhost:8080/api/v1/Empleado/retiveAll",{
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.getItem("sessionToken")}`  // Ejemplo de encabezado de autorización
+      }
+    })
+    employed.data.map((e:any)=>{
+      this.users.push(e)
+      console.log(e)
+    })
+  }
+
+
+  constructor() {
+    this.retriveAllEmployed()
+  }
 
   ngOnInit() {
   }
